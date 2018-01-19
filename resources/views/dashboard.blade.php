7275136edb52172040b296df6d2b60ecfedd0ea3
lengai <371522155@qq.com>
2018-01-19 21:39:11 +0800

    fix bug

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -180,7 +180,7 @@
     <div class="col-md-6">
         <div class="box box-default">
             <div class="box-header with-border">
-                <h3 class="box-title">{{ trans('general.assets') }} by Status</h3>
+                <h3 class="box-title">{{ trans('general.assets') }}状态</h3>
                 <div class="box-tools pull-right">
                     <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
                     </button>
